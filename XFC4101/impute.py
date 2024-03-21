0fd3109393db40159a64ceb2895c047ff08d1a8d
GnohChengYi <chengyignoh@gmail.com>
2024-03-21 16:37:33 +0800

    experiment with varying k=nimp, 2 NANs

--- XFC4101/impute.py
+++ XFC4101/impute.py
@@ -15,18 +15,19 @@ X = np.asarray(keyframes, dtype=np.float32)
 
 # see ForestDiffusion/blob/main/R-Package/ForestDiffusion/man/ForestDiffusion.impute.Rd
 # remove some data to simulate missing data
-mindex = 50 # index of missing data
-X[mindex, 1:3] = (np.nan, np.nan)
+mindexes = (33,66) # index of missing data
+X[mindexes, 1:3] = (np.nan, np.nan)
 print(f'with missing data:\n{X}')
 
 
 # reduce n_t and duplicate_K for faster computation; default: n_t=50, duplicate_K=100
 forest_model = ForestDiffusionModel(X, n_t=50, duplicate_K=100, bin_indexes=[], cat_indexes=[], int_indexes=[0], diffusion_type='vp', n_jobs=-1)
-nimp = 24
+nimp = 32
 # defaults: impute(k=1, X=None, label_y=None, repaint=False, r=5, j=0.1, n_t=None)
-# X_imps = forest_model.impute(k=nimp)    # regular (fast)
+X_imps = forest_model.impute(k=nimp)    # regular (fast)
+# print('imputations:', X_imps)
 
-
+'''
 # trial for each j
 up_10j = 10 # upper bound of 10*j
 diffsqs = np.empty((up_10j, X.shape[1]))  # squared differences
@@ -58,28 +59,42 @@ plt.ylabel('Squared Difference')
 plt.title('Squared Difference vs j')
 plt.legend(['Truth', 'X', 'Y'])
 plt.show()
+'''
 
 
 
 
+# experiment with changing number of imputations
+diffsqs = np.empty((len(X_imps), X.shape[1]))  # all squared differences
+diffsqs_mindex = np.empty((1, X.shape[1]), dtype=np.float32)  # sum of squared differences for each mindex
+for nimp in range(1, len(X_imps) + 1):
+    print('\nnimp:', nimp)
+    diffsqs_mindex.fill(0)
+    for mindex in mindexes:
+        rows = X_imps[:nimp][:, mindex]
+        average = np.average(rows, axis=0)  # take average across imputations to get good imputation
+        print(f'average:\n{average}')
+        truth = [mindex, mindex, mindex]
+        diffsq = (average - truth)**2    # squared difference
+        print(f'diff^2:\n{diffsq}')
+        diffsqs_mindex += diffsq
+        print(f'sum of squared differences:\n{diffsqs_mindex}')
+    diffsqs[nimp - 1] = diffsqs_mindex
+    print(f'overall squared differences:\n{diffsqs}')
+print(f'overall squared differences:\n{diffsqs}')
 
-''' experiment with changing number of imputations
-averages = np.empty((len(rows), X.shape[1]))
-for i in range(len(rows)):
-    averages[i] = np.average(rows[:i+1], axis=0)
-print(averages)
-std_devs = np.std(averages, axis=0) # std_dev of each column of averages
-print(std_devs)
+std_devs = np.std(diffsqs, axis=0) # std_dev of each column of diffsqs
+print(f'standard deviations:\n{std_devs}')
+
 
 # visualization
-x_axis = range(1, len(averages) + 1)
+x_axis = range(1, len(diffsqs) + 1)
 colors = ['red', 'blue', 'green']
 for i in range(X.shape[1]):
-    plt.plot(x_axis, averages[:, i], color=colors[i])
-    plt.errorbar(x_axis, averages[:, i], yerr=std_devs[i], fmt='o', capsize=5, color=colors[i])
+    plt.plot(x_axis, diffsqs[:, i], color=colors[i])
+    plt.errorbar(x_axis, diffsqs[:, i], yerr=std_devs[i], fmt='o', capsize=5, color=colors[i])
 plt.xlabel('Number of Imputations')
-plt.ylabel('Average of Imputations')
-plt.title('Average vs Number of Imputations')
+plt.ylabel('Squared Difference')
+plt.title('Number of Imputations vs Squared Difference')
 plt.legend(['Truth', 'X', 'Y'])
 plt.show()
-'''
