861aa1cea58ac9233f0f1dc42532046005b27bd7
GnohChengYi <chengyignoh@gmail.com>
2024-03-21 19:24:48 +0800

    10% nan; k=6

--- XFC4101/impute.py
+++ XFC4101/impute.py
@@ -26,39 +26,38 @@ print(f'with missing data:\n{X}')
 
 # reduce n_t and duplicate_K for faster computation; default: n_t=50, duplicate_K=100
 forest_model = ForestDiffusionModel(X, n_t=50, duplicate_K=100, bin_indexes=[], cat_indexes=[], int_indexes=[0], diffusion_type='vp', n_jobs=-1)
-nimp = 4  # number of imputations, k: default=1
+nimp = 32  # number of imputations, k: default=1
 # defaults: impute(k=1, X=None, label_y=None, repaint=False, r=5, j=0.1, n_t=None)
-# X_imps = forest_model.impute(k=nimp)    # regular (fast)
+X_imps = forest_model.impute(k=nimp)    # regular (fast)
 # X_imps = forest_model.impute(repaint=True, r=5, j=0.4, k=nimp)   # REPAINT (slow, but better)
 # print('imputations:', X_imps)
 
 
 # trial for each k or r or j
 # up_r = 16  # upper bound of r
-up_10j = 10 # upper bound of 10*j
-avg_diffsqs = np.empty((up_10j, X.shape[1]))  # averages of squared differences
+# up_10j = 10 # upper bound of 10*j
+avg_diffsqs = np.empty((len(X_imps), X.shape[1]))  # averages of squared differences
 diffsqs_mindex = np.empty((1, X.shape[1]), dtype=np.float32)  # sum of squared differences for each mindex
-for ten_j in range(1, up_10j+1):
-    j = ten_j / 10
-    print(f'\n{j}')
-    X_imps = forest_model.impute(repaint=True, j=j, k=nimp)   # REPAINT (slow, but better)
+for iimp in range(1, len(X_imps)+1):
+    # j = ten_j / 10
+    print(f'\n{iimp}')
+    # X_imps = forest_model.impute(repaint=True, j=j, k=nimp)   # REPAINT (slow, but better), comment out for k
     # print('imputations:', X_imps)
     diffsqs_mindex.fill(0)
     for mindex in mindexes:
         # analysis
-        # rows = X_imps[:iimp][:, mindex]   # for varying k
         if nimp == 1:
             rows = X_imps[mindex]
             average = rows
         else:
-            rows = X_imps[:nimp][:, mindex]
+            rows = X_imps[:iimp][:, mindex]
             average = np.average(rows, axis=0)
         print(f'average:\n{average}')
         truth = [mindex, mindex, mindex]
         diffsq = (average - truth)**2    # squared difference
         print(f'diff^2:\n{diffsq}')
         diffsqs_mindex += diffsq
-    avg_diffsqs[ten_j - 1] = diffsqs_mindex / len(mindexes)
+    avg_diffsqs[iimp - 1] = diffsqs_mindex / len(mindexes)
     print(f'average squared differences:\n{avg_diffsqs}')
 
 
@@ -67,14 +66,14 @@ print(f'standard deviations:\n{std_devs}')
 
 
 # visualization
-# x_axis = range(1, len(diffsqs) + 1)   # for varying k
+x_axis = range(1, len(avg_diffsqs) + 1)   # for varying k
 # x_axis = range(1, up_r + 1)
-x_axis = [ten_j / 10 for ten_j in range(1, len(avg_diffsqs) + 1)]
+# x_axis = [ten_j / 10 for ten_j in range(1, len(avg_diffsqs) + 1)]
 colors = ['red', 'blue', 'green']
 for i in range(X.shape[1]):
     plt.plot(x_axis, avg_diffsqs[:, i], color=colors[i])
     plt.errorbar(x_axis, avg_diffsqs[:, i], yerr=std_devs[i], fmt='o', capsize=5, color=colors[i])
-xlabel = 'j'
+xlabel = 'k=nimp'
 plt.xlabel(xlabel)
 plt.ylabel('Average Squared Differences')
 plt.title(f'Average Squared Differences vs {xlabel}')
