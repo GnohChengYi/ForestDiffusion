61ed8b5d0967a67aea22b506967a7fad9b582261
GnohChengYi <chengyignoh@gmail.com>
2024-03-21 19:18:52 +0800

    exp with diff j; 10% nan; j=0.1 best

--- XFC4101/impute.py
+++ XFC4101/impute.py
@@ -33,15 +33,15 @@ nimp = 4  # number of imputations, k: default=1
 # print('imputations:', X_imps)
 
 
-# trial for each r or j
-# up_10j = 10 # upper bound of 10*j
-up_r = 16  # upper bound of r
-avg_diffsqs = np.empty((up_r, X.shape[1]))  # averages of squared differences
+# trial for each k or r or j
+# up_r = 16  # upper bound of r
+up_10j = 10 # upper bound of 10*j
+avg_diffsqs = np.empty((up_10j, X.shape[1]))  # averages of squared differences
 diffsqs_mindex = np.empty((1, X.shape[1]), dtype=np.float32)  # sum of squared differences for each mindex
-for r in range(1, up_r+1):
-    # j = ten_j / 10
-    print('\nr', r)
-    X_imps = forest_model.impute(repaint=True, r=r, k=nimp)   # REPAINT (slow, but better)
+for ten_j in range(1, up_10j+1):
+    j = ten_j / 10
+    print(f'\n{j}')
+    X_imps = forest_model.impute(repaint=True, j=j, k=nimp)   # REPAINT (slow, but better)
     # print('imputations:', X_imps)
     diffsqs_mindex.fill(0)
     for mindex in mindexes:
@@ -58,7 +58,7 @@ for r in range(1, up_r+1):
         diffsq = (average - truth)**2    # squared difference
         print(f'diff^2:\n{diffsq}')
         diffsqs_mindex += diffsq
-    avg_diffsqs[r - 1] = diffsqs_mindex / len(mindexes)
+    avg_diffsqs[ten_j - 1] = diffsqs_mindex / len(mindexes)
     print(f'average squared differences:\n{avg_diffsqs}')
 
 
@@ -67,14 +67,14 @@ print(f'standard deviations:\n{std_devs}')
 
 
 # visualization
-# x_axis = [ten_j / 10 for ten_j in range(1, len(diffsqs) + 1)]
-x_axis = range(1, up_r + 1)
 # x_axis = range(1, len(diffsqs) + 1)   # for varying k
+# x_axis = range(1, up_r + 1)
+x_axis = [ten_j / 10 for ten_j in range(1, len(avg_diffsqs) + 1)]
 colors = ['red', 'blue', 'green']
 for i in range(X.shape[1]):
     plt.plot(x_axis, avg_diffsqs[:, i], color=colors[i])
-    # plt.errorbar(x_axis, diffsqs[:, i], yerr=std_devs[i], fmt='o', capsize=5, color=colors[i])
-xlabel = 'r'
+    plt.errorbar(x_axis, avg_diffsqs[:, i], yerr=std_devs[i], fmt='o', capsize=5, color=colors[i])
+xlabel = 'j'
 plt.xlabel(xlabel)
 plt.ylabel('Average Squared Differences')
 plt.title(f'Average Squared Differences vs {xlabel}')
