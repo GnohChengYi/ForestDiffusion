5ae25b36016921038e303737b439e96379ab74ef
GnohChengYi <chengyignoh@gmail.com>
2024-03-21 15:20:17 +0800

    experiment with varying j; default j=0.1 is not the best; 0.4 is the best (based on k=nimp=1,12,24)

--- XFC4101/impute.py
+++ XFC4101/impute.py
@@ -9,7 +9,7 @@ import pickle
 keyframe_path = 'XFC4101/Preparation/keyframes.pkl'
 with open(keyframe_path, 'rb') as file:
     keyframes = pickle.load(file)
-print('original:', keyframes)
+print(f'original:\n{keyframes}')
 X = np.asarray(keyframes, dtype=np.float32)
 
 
@@ -17,47 +17,45 @@ X = np.asarray(keyframes, dtype=np.float32)
 # remove some data to simulate missing data
 mindex = 50 # index of missing data
 X[mindex, 1:3] = (np.nan, np.nan)
-print('with missing data:', X)
+print(f'with missing data:\n{X}')
 
 
 # reduce n_t and duplicate_K for faster computation; default: n_t=50, duplicate_K=100
-nimp = 12
 forest_model = ForestDiffusionModel(X, n_t=50, duplicate_K=100, bin_indexes=[], cat_indexes=[], int_indexes=[0], diffusion_type='vp', n_jobs=-1)
+nimp = 24
 # defaults: impute(k=1, X=None, label_y=None, repaint=False, r=5, j=0.1, n_t=None)
 # X_imps = forest_model.impute(k=nimp)    # regular (fast)
 
 
-# trial for each r
-up_r = 32
-diffsqs = np.empty((up_r, X.shape[1]))  # squared differences
-for r in range(1, up_r + 1):
-    print('r:', r)
-    X_imps = forest_model.impute(repaint=True, r=r, j=0.1, k=nimp)   # REPAINT (slow, but better)
+# trial for each j
+up_10j = 10 # upper bound of 10*j
+diffsqs = np.empty((up_10j, X.shape[1]))  # squared differences
+for ten_j in range(1, up_10j+1):
+    j = ten_j / 10
+    print('j:', j)
+    X_imps = forest_model.impute(repaint=True, r=5, j=j, k=nimp)   # REPAINT (slow, but better)
     # print('imputations:', X_imps)
 
     # analysis
-    rows = np.empty((nimp, X.shape[1]))
-    for i in range(len(X_imps)):
-        if nimp == 1:
-            # shape of X_imps is different between nimp=1 and nimp>1
-            rows[i] = X_imps[mindex]
-            break
-        rows[i] = X_imps[i][mindex]
+    if nimp == 1:   # shape of X_imps is different between nimp=1 and nimp>1
+        rows = np.array([X_imps[mindex]])
+    else:
+        rows = X_imps[:, mindex]
     print(f'interest:\n{rows}')
     average = np.average(rows, axis=0)
     print(f'average:\n{average}')
     truth = [mindex, mindex, mindex]
     diffsq = (average - truth)**2    # squared difference
     print(f'diff^2:\n{diffsq}')
-    diffsqs[r-1] = diffsq
-
+    diffsqs[ten_j - 1] = diffsq
+print(f'squared differences:\n{diffsqs}')
 
 # visualization
-x_axis = range(1, len(diffsqs) + 1)
+x_axis = [ten_j / 10 for ten_j in range(1, len(diffsqs) + 1)]
 plt.plot(x_axis, diffsqs)
-plt.xlabel('r')
+plt.xlabel('j')
 plt.ylabel('Squared Difference')
-plt.title('r vs Squared Difference')
+plt.title('Squared Difference vs j')
 plt.legend(['Truth', 'X', 'Y'])
 plt.show()
 
